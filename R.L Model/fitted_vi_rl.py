996c7a2140ed4d4d5b8b44cc821a674a2384e0ab
ayushL2007 <ayushlahiri2008@gmail.com>
2026-06-18 17:57:13 +0530

    1.Added Optimal Action and Value predicition fn\n2.Fixed dimension erros

--- R.L Model/fitted_vi_rl.py
+++ R.L Model/fitted_vi_rl.py
@@ -6,23 +6,26 @@ import reward
 
 import numpy as np
 import einops as eops
-
 class RL():
     def __init__(self, _nstates :int, _nactions :int, rewardFn_):
         self.theta = np.zeros(shape=(1, _nstates))
         self._nState = _nstates
-        self._nActions = _nactions
-        self.reward_fn = lambda state : rewardFn_(state)
+        self._nAction = _nactions
+        self.reward_fn = lambda state, action : rewardFn_(state, action)
 
-    def runSGD(self, y_i :np.ndarray, _state :np.ndarray):
-        diff = 2 * self.theta @ _state - y_i
+    def runSGD(self, y_i :np.ndarray, _state :np.ndarray, theta):
+        diff = 2 * (theta @ _state) - y_i
         grad = diff * _state
-        self.theta -= self.lr*grad
+        # print(diff.shape, _state.shape, y_i.shape, grad.shape)
+        self.theta -= self.lr*grad.T
+        return theta
+        
+        
 
     #Follows Fitted value iteration
-    def train(self, States :np.ndarray, Actions :np.ndarray, lr :float=1e-2, epochs :int=1000, gamma :float=0.99):
-        ns_pred_model = NextStateModel(self._nState, self._nActions)
-        ns_pred_model.train(States, Actions, epochs)
+    def train(self, States :np.ndarray, Actions :np.ndarray, lr :float=1e-4, epochs :int=1000, gamma :float=0.99):
+        self.ns_pred_model = lr_model
+        model = self.ns_pred_model
         
         if States.ndim != 2 or Actions.ndim != 2:
             print("Dimension mismatch")
@@ -36,20 +39,63 @@ class RL():
             print(f"Req:({self._nState},{self._nAction}) provided:({_nState},{_nAction})")
             return
 
-        theta = self.theta
-        self.lr = lr
         
+        self.lr = lr
+
         for iter_ in range(epochs):
+            s_i = (_nSample//epochs)*iter_
+            print(f"running {iter_+1}th epoch at loss:{self.acc(States[:,0:1], Actions[:, 0:1])}")
+            print(f"Updating theta from {self.theta}", end="")
+            theta = self.theta.copy()
+            
+            skipped=0
             for i in range(_nSample):
+                pass_or_not = np.random.rand(1)
+                if pass_or_not>0.5:
+                    skipped+=1
+                    continue
                 state = States[:, i:i+1]
+                actions = np.arange(-3.0, 4, 1).reshape(7,1)
                 
-                ns_pred = np.array([model.__predict__(state, action).flatten() for action in np.arange(-3, 3.25, 0.25)])
-                rewards = self.reward_fn(state)
+                ns_pred = np.array([model.__predict__(state, actions[action][:].reshape(1,1)) for action in range(actions.shape[0])])
                 
-                print(ns_pred.shape)
-                
-                q_a = np.array([rewards+gamma*(theta@ns) for ns in ns_pred])
-                print(q_a.shape)
+                rewards = self.reward_fn(state, Actions[:,i:i+1])
+                ns_pred = eops.rearrange(ns_pred, "r c e-> c (r e)")
+                q_a = np.array([rewards+gamma*(self.theta@ns_pred[:,ns:ns+1]) for ns in range(ns_pred.shape[1])])
                 y_i = np.max(q_a)
                 
-                self.runSGD(y_i, state)
+                self.runSGD(y_i, state, theta)
+
+            theta = self.theta.copy()
+            
+            print(f" to {self.theta}")
+            print(f"skipped: {skipped} data pts")
+
+
+
+    def acc(self, state, action):
+        pred_action = self.predOptimalAction(state)
+        diff = np.abs(pred_action-action)
+        acc = (diff**2).mean()
+        return acc
+
+    def predOptimalState(self, State :np.ndarray):
+        if State.shape[0]!=self._nState:
+            print("Mismatching Dimension")
+            return
+
+        return self.theta@State
+                                   
+    def predOptimalAction(self, State :np.ndarray):
+        possible_actions = np.arange(-3.0, 4, 1).reshape(7,1)
+        
+        possible_ns_ = [self.ns_pred_model.__predict__(State, possible_actions[i:i+1, :]) 
+                        for i in range(possible_actions.shape[0])]
+        
+        # Calculate the Value of each predicted next state
+        values = [self.theta @ ns for ns in possible_ns_]
+
+        print(values)
+        # The optimal action is the one that leads to the highest future value
+        best_action_idx = np.argmax(values)
+        return possible_actions[best_action_idx]
